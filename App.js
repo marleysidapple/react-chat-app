bedf62a4e84d3b01e3242176e4ab59ad43f07f6a
marleysidapple <bsiddhartha25@gmail.com>
2017-07-03 11:27:16 +0800

    fix

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import from './img/back.jpg';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import './img/back.jpg';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload. Oh. is that so</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+       <Image source={require('./img/back.jpg')} >
+          <Text style={styles.tef}>adasd</Text>
+       </Image>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
