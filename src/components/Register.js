f2e253f87335cee63ec8b47cf77b0c40434fa6ee
marleysidapple <bsiddhartha25@gmail.com>
2017-07-09 12:30:44 +0800

    added register view

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import baseStyle from './../../assets/css/globalcss';
+import Input from './../common/Input';
+import Button from './../common/Button';
+
 
 class Register extends Component {
 
@@ -11,10 +14,25 @@ class Register extends Component {
 	render(){
 		return(
 			<View style={baseStyle.container}>
-				<Text>Welcome to register</Text>
+				<Text style={styles.mainHeader}>Euleo</Text>
+				<Input placeholder={'your full name'} iconName={'user'} />
+				<Input placeholder={'your email'} iconName={'envelope-o'} />
+				<Input placeholder={'password'} iconName={'key'} />
+				<Input placeholder={'Retype Password'} iconName={'key'} />
+				  <Button buttonTitle={'Register'} />
+
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	mainHeader: {
+    fontSize: 28,
+    marginBottom: 25,
+    color: '#605a5a',
+    textAlign: 'center',
+  },
+});
 
 export default Register;
