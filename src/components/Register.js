3fe23d9af2e7a4be933c024fe7f9a25ff5ff87e1
marleysidapple <bsiddhartha25@gmail.com>
2017-07-12 15:55:04 +0800

    added scrollable

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, ScrollView, StyleSheet } from 'react-native';
 import baseStyle from './../../assets/css/globalcss';
 import { handleRegistrationForm } from './../actions/Auth';
 import Input from './../common/Input';
@@ -20,40 +20,40 @@ class Register extends Component {
 
 	render(){
 		return(
-			<View style={globalcss.container}>
-				<Text style={styles.mainHeader}>Euleo</Text>
-				<Input 
-					placeholder={'your full name'} 
-					iconName={'user'} 
-					value={this.props.fullname} 
-					onInputChange={(value) => this.props.handleRegistrationForm({prop: 'fullname', value: value})} />
+			<ScrollView contentContainerStyle={globalcss.container}>
+					<Text style={styles.mainHeader}>Euleo</Text>
+					<Input 
+						placeholder={'your full name'} 
+						iconName={'user'} 
+						value={this.props.fullname} 
+						onInputChange={(value) => this.props.handleRegistrationForm({prop: 'fullname', value: value})} />
 
 
-				<Input placeholder={'your email'} 
-						iconName={'envelope-o'} 
-						value={this.props.email} 
-						onInputChange={(value) => this.props.handleRegistrationForm({prop: 'email', value: value})} />
+					<Input placeholder={'your email'} 
+							iconName={'envelope-o'} 
+							value={this.props.email} 
+							onInputChange={(value) => this.props.handleRegistrationForm({prop: 'email', value: value})} />
 
 
-				<Input 
-					placeholder={'password'} 
-					iconName={'key'} 
-					value={this.props.password} 
-					secureTextEntry={true}
-					onInputChange={(value) => this.props.handleRegistrationForm({prop: 'password', value: value})}
-					 />
+					<Input 
+						placeholder={'password'} 
+						iconName={'key'} 
+						value={this.props.password} 
+						secureTextEntry={true}
+						onInputChange={(value) => this.props.handleRegistrationForm({prop: 'password', value: value})}
+						 />
 
 
-				<Input 
-					placeholder={'Retype Password'} 
-					iconName={'key'} 
-					value={this.props.confirm_password} 
-					secureTextEntry={true}
-					onInputChange={(value) => this.props.handleRegistrationForm({prop: 'confirm_password', value: value})} 
-					/>
-				
-				<Button buttonTitle={'Register'} onPress={this.onRegisterPress}/>
-			</View>
+					<Input 
+						placeholder={'Retype Password'} 
+						iconName={'key'} 
+						value={this.props.confirm_password} 
+						secureTextEntry={true}
+						onInputChange={(value) => this.props.handleRegistrationForm({prop: 'confirm_password', value: value})} 
+						/>
+					
+					<Button buttonTitle={'Register'} onPress={this.onRegisterPress}/>
+			</ScrollView>
 		);
 	}
 }
