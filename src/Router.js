e64b7aeecc0c44fa8330cbd251b00ca1b7b85db6
marleysidapple <bsiddhartha25@gmail.com>
2017-07-18 09:59:58 +0800

    added some layout in dashboard

--- src/Router.js
+++ src/Router.js
@@ -16,12 +16,10 @@ class RouterComponent extends Component{
 		return(
 			<Router sceneStyle={globalcss.sceneWrapper}>
 				<Scene key="auth">
-	 				<Scene key="login" component={Login} title={'Please Login'} initial/>
+	 				<Scene key="login" component={Login} title={'Please Login'} />
 	 				<Scene key="register" component={Register} title={'Please Register'}/>
-	 				<Scene key="dashboard" component={Dashboard} title={'Some description'} />		
+	 				<Scene key="dashboard" component={Dashboard} title={'Some description'} initial/>		
 	 			</Scene>
-
-	 			
 
 			</Router>
 		);
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Scene } from 'react-native-router-flux';
+import globalcss from './../../assets/css/globalcss';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Dashbaord extends Component {
 
@@ -9,10 +12,64 @@ class Dashbaord extends Component {
 
 	render(){
 		return(
-			<Text>This is Dashbaord</Text>
+
+
+			<View style={styles.container}>
+				<View>
+					<Text>View One</Text>
+				</View>
+
+
+
+				<View style={styles.footerStyle}>
+					<View>
+						<Icon name={'commenting-o'} size={22} color="#605a5a" />
+						<Text>Chats</Text>
+					</View>
+
+					<View>
+						<Icon name={'commenting-o'} size={22} color="#605a5a" />
+						<Text>Chats</Text>
+					</View>
+
+					<View>
+						<Icon name={'commenting-o'} size={22} color="#605a5a" />
+						<Text>Chats</Text>
+					</View>
+
+					<View>
+						<Icon name={'commenting-o'} size={22} color="#605a5a" />
+						<Text>Chats</Text>
+					</View>
+					
+
+				</View>
+			</View>
+    
 		);
 	}
 
 }
+
+const styles = StyleSheet.create({
+	container: {
+	    backgroundColor: '#f7f4f4',
+	    justifyContent: 'space-between',
+	    flex: 1,
+  },
+
+  footerStyle: {
+  	flexDirection: 'row',
+  	height: 55,
+  	backgroundColor: '#d1cfcf',
+  	padding: 10,
+  	justifyContent: 'space-between',
+  	borderTopWidth : .5,
+    borderColor    : '#b7b7b7',
+    opacity: 1
+  }
+
+
+});
 
 export default Dashbaord; 
